a02bf4c63235ac06d9392a46f23a0304d89b40cc
Tyler Long <tyler4long@gmail.com>
2020-09-18 15:48:10 -0700

    csv parse

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import RingCentral from '@rc-ex/core';
 import EngageVoiceExtension from '@rc-ex/engage-voice';
+import fs from 'fs';
+import papaparse from 'papaparse';
 
 const rc = new RingCentral({
   clientId: process.env.ENGAGE_VOICE_CLIENT_ID!,
@@ -19,8 +21,14 @@ const rc = new RingCentral({
   await rc.installExtension(engageVoiceExtension);
   await engageVoiceExtension.authorize();
 
-  const r = await engageVoiceExtension.get('/voice/api/v1/admin/accounts');
-  console.log(r.data);
+  const csvString = fs.readFileSync(
+    process.env.GOOGLE_FORMS_CSV_FILE!,
+    'utf-8'
+  );
+  const rows = papaparse.parse(csvString).data;
+  console.log(rows);
+  // const r = await engageVoiceExtension.get('/voice/api/v1/admin/accounts');
+  // console.log(r.data);
 
   await rc.revoke();
 })();
